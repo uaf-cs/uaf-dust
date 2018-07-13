956841feb8e62eed1c7a1542684052ba86e0c0b5
Diane <dmmurph@alaska.edu>
2018-07-12 18:28:15 -0800

    got a graph up! Yay!

--- dust.js
+++ dust.js
@@ -3,4 +3,11 @@ class Dust {
     constructor() {
     }
 }
+class GraphApp {
+    constructor() {
+        let e = document.createElement('div');
+        e.innerHTML = "Hello, world";
+        document.body.appendChild(e);
+    }
+}
 //# sourceMappingURL=dust.js.map
--- graph-app/graph-app.js
+++ graph-app/graph-app.js
@@ -2,7 +2,7 @@
 class GraphApp {
     constructor() {
         let e = document.createElement('div');
-        e.innerHTML = "Hello, world";
+        e.innerHTML = "Hello, world again";
         document.body.appendChild(e);
     }
 }
