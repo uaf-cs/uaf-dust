f7e1c1e26b79f109e68445d6e1d520c32e14dfab
Jonathan Metzgar <jonathan@new-host-3.home>
2019-02-10 17:04:48 -0900

    modify base url

--- frontend/src/app/serviceUrls.ts
+++ frontend/src/app/serviceUrls.ts
@@ -5,5 +5,5 @@ export const PalliativesUrl = '/palliatives';
 export const AuthUrl = '/auth';
 export const PhpServiceUrl = '/service.php';
 export const PhpAdminUrl = '/admin.php';
-export const BaseServiceUrl = 'https://snow.ine.uaf.edu/~jbmetzgar/uafdust';
+export const BaseServiceUrl = 'https://snow.ine.uaf.edu/~uafdust';
 export const DevServiceUrl = 'http://localhost:8000';
